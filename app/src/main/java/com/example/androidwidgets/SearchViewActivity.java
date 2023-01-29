ab0bfbf8293cbe4a2faa28a1eebfaecb97681f96
Anupama <anupama.lakshmi@Improve10x.com>
2023-01-29 16:48:52 +0530

    I Completed the functionality of SearchViewActivity

--- app/src/main/java/com/example/androidwidgets/SearchViewActivity.java
+++ app/src/main/java/com/example/androidwidgets/SearchViewActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.widget.SearchView;
 import android.os.Bundle;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.androidwidgets.databinding.ActivitySearchViewBinding;
 
@@ -14,8 +15,6 @@ import java.util.ArrayList;
 public class SearchViewActivity extends AppCompatActivity {
 
     private ActivitySearchViewBinding binding;
-    private SearchView searchView;
-    private ListView listView;
     private ArrayList<String> list;
     private ArrayAdapter<String> arrayAdapter;
 
@@ -24,5 +23,38 @@ public class SearchViewActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivitySearchViewBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+        setUpData();
+        setSearchView();
+    }
+
+    private void setUpData() {
+        list = new ArrayList<>();
+        list.add("Apple");
+        list.add("Banana");
+        list.add("Pineapple");
+        list.add("Orange");
+        list.add("Water Melon");
+        list.add("Papaya");
+    }
+
+    private void setSearchView() {
+        arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, list);
+        binding.listViewLv.setAdapter(arrayAdapter);
+        binding.searchViewSv.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+               if(list.contains(query)) {
+                   arrayAdapter.getFilter().filter(query);
+               } else {
+                   Toast.makeText(SearchViewActivity.this, "Not Match found", Toast.LENGTH_SHORT).show();
+               }
+               return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
     }
 }
